4a280c0a80899538e6c32884a81c8bafb654754e
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2018-07-04 13:03:25 -0300

    challenge-31

--- js/script.js
+++ js/script.js
@@ -1,36 +1,5 @@
 (function($, doc) {
   'use strict';
-
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-  Essas informações devem ser adicionadas no HTML via Ajax.
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
-
   function app(){
 
     var image =  $('[data-js="image"]').get();
@@ -43,6 +12,7 @@
       init: function init(){
         this.companyInfo();
         this.initEvents();
+        this.getRemoveTd();
       },
 
       initEvents: function initEvents(){
@@ -64,8 +34,8 @@
         var $tdYear = doc.createElement('td');
         var $tdPlate = doc.createElement('td');
         var $tdColor = doc.createElement('td');
-
         var $img = doc.createElement('img');
+
         $img.src = image.value;
         $tdImage.appendChild($img);
 
@@ -111,6 +81,17 @@
 
       isRequestOK: function isRequestOK(){
         return this.readyState === 4 && this.status === 200;
+      },
+
+      getRemoveTd: function getRemoveTd(){
+        $('[data-js="remove"]').on('click', this.removeTd);
+      },
+
+      removeTd: function removeTd(event){
+        event.preventDefault();
+        var $tabCarro = $('[data-js="table-car"]');
+        var $trCar = $tabCarro.lastElementChild;
+        $trCar.innerHTML = '';
       }
     }
 
